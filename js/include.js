53b796830ffb768537aec4b1b513c6374c86504e
jaurarav <ravneet.jaura@mail.utoronto.ca>
2026-04-21 16:06:50 -0400

    fixed header and footer

--- js/include.js
+++ js/include.js
@@ -39,33 +39,33 @@ function setActiveNav() {
     // 2) Get all links in the navigation
     const links = document.querySelectorAll(".nav-links a");
 
-
-    </script>
     // 3) Compare each link file name with the current page
     links.forEach((link) => {
-        // Get the link target from href
-        const href = link.getAttribute("href") || "";
+            // Get the link target from href
+            const href = link.getAttribute("href") || "";
 
 
-        // Example: ../pages/about.html -> about.html
-        let linkPage = href.split("/").pop() || "";
+            // Example: ../pages/about.html -> about.html
+            let linkPage = href.split("/").pop() || "";
 
 
-        // Remove query/hash from the link too
-        linkPage = linkPage.split("?")[0].split("#")[0];
+            // Remove query/hash from the link too
+            linkPage = linkPage.split("?")[0].split("#")[0];
 
 
-        // Clear old active states first
-        link.classList.remove("active");
+            // Clear old active states first
+            link.classList.remove("active");
 
 
-        // If this link matches the current page, highlight it
-        if (linkPage === currentPage) {
-            link.classList.add("active");
-        }
-    });
+            // If this link matches the current page, highlight it
+            if (linkPage === currentPage) {
+                link.classList.add("active");
+            }
+        });
 }
 
 
 // Run the function when the page loads
-loadIncludes();
+loadIncludes().then(() => {
+    setActiveNav();
+});
